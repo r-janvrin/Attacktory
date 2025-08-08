cb022beb259e83002aebad9a61fc8657d109e1c1
r-janvrin <ryan.janvrin@outlook.com>
2025-08-07 22:16:56 -0400

    created a display showing where the building will be placed

--- Assets/Scripts/mouseScript.cs
+++ Assets/Scripts/mouseScript.cs
@@ -4,14 +4,18 @@ using static Unity.Collections.AllocatorManager;
 
 public class testScript : MonoBehaviour
 {
-
     [SerializeField] private InputActionReference click;
     [SerializeField] private InputActionReference scroll;
     [SerializeField] private GameObject currentBuilding;
     [SerializeField] private byte currentSize;
     [SerializeField] private bool validPosition;
-    public float scrollValue;
+    [SerializeField] private GameObject hoverObject;
+    private SpriteRenderer hoverSprite;
 
+    private Color whiteColor;
+    private Color redColor;
+
+    public float scrollValue;
     private int currentRotation;
     private Quaternion[] rotations;
 
@@ -20,7 +24,11 @@ public class testScript : MonoBehaviour
     Vector2Int gridPosition;
     private void Awake()
     {
+        //create the 2 colors: white is slightly transparent, red is red & transparent
+        whiteColor = new Color(1, 1, 1, 0.8f);
+        redColor = new Color(1, 0.35f, 0.35f, 0.8f);
         currentSize = currentBuilding.GetComponent<baseBuildingScript>().buildingSize;
+        hoverSprite = hoverObject.GetComponent<SpriteRenderer>();
         int degrees = 0;
         rotations = new Quaternion[4];
         for(int i = 0; i < 4; i++)
@@ -31,13 +39,26 @@ public class testScript : MonoBehaviour
     }
     void Update()
     {
+        //check to see if scroll up or down and rotate based on that
         scrollValue = scroll.action.ReadValue<float>();
         if (scrollValue > 0) ScrollUp();
         else if (scrollValue < 0) ScrollDown();
+
+        //get where the mouse is
         mousePosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
         gridPosition = TileDataManager.manager.getVector2Int(mousePosition) - sizeOffset;
         validPosition = areaIsValid(gridPosition, currentSize);
-        //Debug.Log(validPosition);
+
+        //show where the building will be built and if it can be built
+        hoverObject.transform.position = gridPosition + new Vector2(currentSize * 0.5f, currentSize * 0.5f);
+        if (validPosition)
+        {
+            hoverSprite.color = whiteColor;
+        }
+        else
+        {
+            hoverSprite.color = redColor;
+        }
     }
 
     private void OnEnable()
@@ -66,6 +87,7 @@ public class testScript : MonoBehaviour
         Debug.Log("Scrolled Up!");
         currentRotation++;
         if (currentRotation > 3) currentRotation = 0;
+        hoverObject.transform.rotation = rotations[currentRotation];
     }
 
     private void ScrollDown()
@@ -73,6 +95,7 @@ public class testScript : MonoBehaviour
         Debug.Log("Scrolled down!");
         currentRotation--;
         if (currentRotation < 0) currentRotation = 3;
+        hoverObject.transform.rotation = rotations[currentRotation];
     }
 
 
@@ -89,5 +112,6 @@ public class testScript : MonoBehaviour
         currentBuilding = prefab;
         currentSize = currentBuilding.GetComponent<baseBuildingScript>().buildingSize;
         sizeOffset = new Vector2Int(currentSize / 2, currentSize / 2);
+        hoverSprite.sprite = currentBuilding.GetComponent<SpriteRenderer>().sprite;
     }
 }
