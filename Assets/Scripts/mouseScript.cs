4f18049a8cc0488ade6bc36d66a325747560478b
r-janvrin <ryan.janvrin@outlook.com>
2025-08-25 19:20:57 -0400

    Added more UI panels and added full planned building functionality

--- Assets/Scripts/mouseScript.cs
+++ Assets/Scripts/mouseScript.cs
@@ -13,6 +13,7 @@ public class mouseScript : MonoBehaviour
     [SerializeField] private byte currentSize;
     [SerializeField] private bool validPosition;
     [SerializeField] private GameObject hoverObject;
+    [SerializeField] private GameObject hoverPrefab;
     private SpriteRenderer hoverSprite;
     private bool isMouseDown;
     private bool isOverUI;
@@ -36,7 +37,7 @@ public class mouseScript : MonoBehaviour
         //create the 2 colors: white is slightly transparent, red is red & transparent
         whiteColor = new Color(1, 1, 1, 0.8f);
         redColor = new Color(1, 0.35f, 0.35f, 0.8f);
-        plannedBuildings = new creationQueue(500);
+        plannedBuildings = new creationQueue(500, whiteColor, redColor);
         currentSize = currentBuilding.GetComponent<baseBuildingScript>().buildingSize;
         hoverSprite = hoverObject.GetComponent<SpriteRenderer>();
         int degrees = 0;
@@ -81,11 +82,12 @@ public class mouseScript : MonoBehaviour
         Vector2Int mouseDirection = getMouseDirection(out numBuildings);
         if(lastDirection == mouseDirection) //if we're in the same direction
         {
-            plannedBuildings.setNumBuildings(numBuildings, currentBuilding);
+            plannedBuildings.setNumBuildings(numBuildings, hoverPrefab, hoverSprite.sprite);
             return;
         }
         //get rid of all old buildings and add all the new ones
-        plannedBuildings.changeDirection(mouseDirection, numBuildings, hoverObject);
+        plannedBuildings.changeDirection(mouseDirection, numBuildings, hoverPrefab, hoverSprite.sprite);
+        plannedBuildings.setValidColors();
     }
 
     private void OnEnable()
@@ -106,16 +108,20 @@ public class mouseScript : MonoBehaviour
 
     private void ClickStart(InputAction.CallbackContext obj)
     {
+        hoverObject.SetActive(false);
         if (isOverUI || currentBuilding == null) return;//if its a UI click or no choice, don't build
         //getting values of start of click
         isMouseDown = true;
         clickPosition = gridPosition;
-        plannedBuildings.setPosition(new Vector2(gridPosition.x + currentSize * 0.5f, gridPosition.y + currentSize * 0.5f));
+        plannedBuildings.setPosition(gridPosition);
         lastDirection = Vector2.zero;
+        Debug.Log(mousePosition);
+        Debug.Log(clickPosition);
     }
 
     private void ClickEnd(InputAction.CallbackContext obj)
     {
+        hoverObject.SetActive(true);
         isMouseDown = false;
         //put all our planned buildings to the building manager
         Vector2Int tempPosition = clickPosition;
@@ -143,24 +149,25 @@ public class mouseScript : MonoBehaviour
     }
 
     public Vector2Int getMouseDirection(out int numBuildings){
-        Vector2 mouseOffset = mousePosition - clickPosition;// + new Vector2(currentSize * 0.5f, currentSize * 0.5f);
-
+        //get offset from middle of building
+        Vector2 mouseOffset = mousePosition - clickPosition - new Vector2(currentSize * 0.5f, currentSize * 0.5f);
+        Debug.Log(mouseOffset + "" + clickPosition);
 
         //if we are more horizontal than vertical
         if (Mathf.Abs(mouseOffset.x) > Mathf.Abs(mouseOffset.y))
         {
-            numBuildings = 1 + (int)(Mathf.Abs(mouseOffset.x)) / currentSize;
+            numBuildings = (int)Mathf.Ceil(( (Mathf.Abs(mouseOffset.x) + currentSize * 0.5f)/ currentSize));
             if (mouseOffset.x > 0) return Vector2Int.right;
-            numBuildings++;
+            //numBuildings++;
             return Vector2Int.left;
         }
-        numBuildings = 1 + (int)(Mathf.Abs(mouseOffset.y)) / currentSize;
+        numBuildings = (int)Mathf.Ceil(((Mathf.Abs(mouseOffset.y) + currentSize * 0.5f) / currentSize));
         if (mouseOffset.y > 0) return Vector2Int.up;
-        numBuildings++;
+        //numBuildings++;
         return Vector2Int.down;
 
     }
-    public bool areaIsValid(Vector2Int bottomLeft, byte size)
+    public static bool areaIsValid(Vector2Int bottomLeft, byte size)
     {
         if (!buildingGrid.grid.tilesAreEmpty(bottomLeft, size)) return false; //make sure there's no buildings
         if (!TileDataManager.manager.tilesAreValid(bottomLeft, size)) return false; //make sure there's no walls
@@ -191,29 +198,36 @@ class creationQueue
     public int numPlanned;
     public GameObject[] buildings;
     public Vector2Int direction;
-    private Vector2 position;
+    private Vector2 creationPosition;
+    private Vector2Int BottomLeftPosition;
     private Quaternion rotation;
-    private int buildingSize;
+    private byte buildingSize;
     private GameObject prefab;
-    public creationQueue(int sizeArray)
+    private Color whiteColor;
+    private Color redColor;
+    public creationQueue(int sizeArray, Color whiteColor, Color redColor)
     {
         numPlanned = 0;
         buildings = new GameObject[sizeArray];
         rotation = Quaternion.identity;
         buildingSize = 1;
+        this.whiteColor = whiteColor;
+        this.redColor = redColor;
     }
-    public void setPosition(Vector2 newPosition)
+    public void setPosition(Vector2Int BL_Position)
     {
-        position = newPosition;
+        BottomLeftPosition = BL_Position;
+        creationPosition = BottomLeftPosition + new Vector2(buildingSize * 0.5f, buildingSize * 0.5f);
     }
-    public void setBuildingSize(int newSize)
+    public void setBuildingSize(byte newSize)
     {
         buildingSize = newSize;
     }
 
-    public void addToQueue(GameObject objToAdd)
+    public void addToQueue(GameObject objToAdd, Sprite sprite)
     {
-        buildings[numPlanned] = GameObject.Instantiate(objToAdd, position + direction * numPlanned, rotation);
+        buildings[numPlanned] = GameObject.Instantiate(objToAdd, creationPosition + direction * numPlanned * buildingSize, rotation);
+        buildings[numPlanned].GetComponent<SpriteRenderer>().sprite = sprite;
         numPlanned++;
     }
     public void setRotation(Quaternion targetRotation)
@@ -234,12 +248,12 @@ class creationQueue
     {
         while (numPlanned > 0) removeFromQueue();
     }
-    public void setNumBuildings(int howMany, GameObject objToAdd)
+    public void setNumBuildings(int howMany, GameObject objToAdd, Sprite sprite)
     {
         if (howMany < 0) return;
         while(howMany > numPlanned)
         {
-            addToQueue(objToAdd);
+            addToQueue(objToAdd, sprite);
         }
             //remove
         while(numPlanned > howMany)
@@ -247,10 +261,18 @@ class creationQueue
             removeFromQueue();
         }
     }
-    public void changeDirection(Vector2Int newDirection, int howMany, GameObject objToAdd)
+    public void changeDirection(Vector2Int newDirection, int howMany, GameObject objToAdd, Sprite sprite)
     {
         direction = newDirection;
         clearQueue();
-        setNumBuildings(howMany, objToAdd);
+        setNumBuildings(howMany, objToAdd, sprite);
+    }
+
+    public void setValidColors()
+    {
+        for(int i = 0; i < numPlanned; i++)
+        {
+            buildings[i].GetComponent<SpriteRenderer>().color = mouseScript.areaIsValid(BottomLeftPosition + direction * i * buildingSize, buildingSize) ? whiteColor : redColor;
+        }
     }
 }
